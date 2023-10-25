9eec194bf434aa4794a2a1dd101c2adcf3fd18ae
HydroOx1d <n-esenov44@mail.ru>
2023-10-26 02:02:16 +0600

    AppLink widget

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import cls from './AppLink.module.scss'
+import { LinkProps, Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames';
+
+export enum AppLinkTheme {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary'
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme
+}
+
+export const AppLink: React.FC<AppLinkProps> = (props) => {
+  const {
+    className,
+    children,
+    to,
+    theme = AppLinkTheme.PRIMARY,
+    ...otherProps
+  } = props;
+
+  return (
+    <Link to={to} className={classNames(cls.AppLink, {}, [className, cls[theme]])} {...otherProps}>
+      {children}
+    </Link>
+  );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames';
 import cls from './Navbar.module.scss';
 import ThemeSwitcher from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 
 const Navbar = () => {
   return (
     <nav className={classNames(cls.Navbar)}>
       <ThemeSwitcher/>
       <div className={cls.links}>
-        <Link to={"/"}>Главная</Link>
-        <Link to={"/about"}>О нас</Link>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>Главная</AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>О нас</AppLink>
       </div>
     </nav>
   );
