94910db2022cc914c5174f0008da1eafdd698c4a
HydroOx1d <n-esenov44@mail.ru>
2023-10-28 16:41:20 +0600

    jest

---
+++ src/shared/lib/classNames/className.test.ts
@@ -0,0 +1,47 @@
+import { classNames } from "./className";
+
+describe("className", () => {
+	test("one argument", () => {
+		const expectation = "class";
+		expect(classNames("class")).toBe(expectation);
+	});
+
+	test("argument in additional", () => {
+		const expectation = "class class1 class2";
+		expect(classNames("class", {}, ["class1", "class2"])).toBe(expectation);
+	});
+
+	test("argument in mods", () => {
+		const expectation = "class class1 class2 hovered selected";
+
+		expect(
+			classNames("class", { hovered: true, selected: true }, [
+				"class1",
+				"class2",
+			])
+		).toBe(expectation);
+	});
+
+	test("mods argument false", () => {
+		const expectation = "class class1 class2 hovered";
+
+		expect(
+			classNames("class", { hovered: true, selected: false }, [
+				"class1",
+				"class2",
+			])
+		).toBe(expectation);
+	});
+
+
+	test("mods argument undefined", () => {
+		const expectation = "class class1 class2 hovered";
+
+		expect(
+			classNames("class", { hovered: true, selected: undefined }, [
+				"class1",
+				"class2",
+			])
+		).toBe(expectation);
+	});
+});
