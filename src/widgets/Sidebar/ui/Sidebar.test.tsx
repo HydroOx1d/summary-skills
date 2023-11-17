5252476b045a8d3166255fd2dd5c24725b319a43
HydroOx1d <n-esenov44@mail.ru>
2023-11-17 22:36:36 +0600

    delete an unnecessary folder (Sidebar)

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, screen } from "@testing-library/react";
 import { renderWithTranslations } from "shared/lib/tests/renderWithTranslations/renderWithTranslations";
-import Sidebar from "widgets/Sidebar/ui/Sidebar/Sidebar";
+import Sidebar from "widgets/Sidebar/ui/Sidebar";
 
 describe("Sidebar", () => {
 	test("first render component", () => {
