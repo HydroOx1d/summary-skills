1a3fba43e60e5dd265670e591c9d305d6cc49c15
HydroOx1d <escoreze1773@gmail.com>
2024-07-04 17:11:51 +0600

    add alias eslint custom plugin

--- config/eslint/packages/eslint-plugin-fsd-import-checker/index.js
+++ config/eslint/packages/eslint-plugin-fsd-import-checker/index.js
@@ -1,64 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const RuleTester = require("eslint").RuleTester;
-const Path = require("path");
-
-const layers = new Set(["features", "entity", "pages", "widgets"]);
-
-function isRelative(path) {
-	return path.startsWith("../") || path.startsWith("./");
-}
-
-// filename: example - \\?\C:\Users\User\Desktop\projects\production-project\src\pages\about\ui\About.tsx
-// path: example - features/test/model/types
-
-function shouldBeRelative(filename, path) {
-	if (isRelative(path)) {
-		return false;
-	}
-
-	
-	const pathArray = path.split("/");
-	
-	const pathLayer = pathArray[0];
-	const pathSlice = pathArray[1];
-	
-	if (!pathLayer || !pathSlice || !layers.has(pathLayer)) {
-		return false;
-	}
-
-	const filenameArray = filename.split("src")[1].split(Path.sep).filter(Boolean);
-
-	const filenameLayer = filenameArray[0];
-	const filenameSlice = filenameArray[1];
-
-	if (!filenameLayer || !filenameSlice || !layers.has(filenameLayer)) {
-		return false;
-	}
-
-	return pathLayer === filenameLayer && pathSlice === filenameSlice;
-}
+const checkRule = require("./lib/rules/check.js");
 
 const plugin = {
 	meta: {
 		name: "eslint-plugin-fsd-import-checker",
-		version: "1.0.3"
+		version: "1.0.7"
 	},
-	rules: {
-		"check": {
-			create: (ctx) => {
-				return {
-					ImportDeclaration(node) {
-						const path = node.source.value.replace("@/", "");
-						const filename = ctx.filename;
 
-						if (shouldBeRelative(filename, path)) {
-							ctx.report(node, "This import should be relative");
-						}
-					}
-				};
-			}
-		}
+	rules: {
+		"check": checkRule
 	},
 };
 
@@ -66,15 +18,20 @@ const ruleTester = new RuleTester({
 	parserOptions: { ecmaVersion: "latest", sourceType: "module" }
 });
 
-ruleTester.run("check", plugin.rules.check, {
+ruleTester.run("check", checkRule, {
 	valid: [
 		{
 			code: "import Test from \"features/test\"",
-			filename: "C:\\Users\\User\\Desktop\\projects\\production-project\\src\\pages\\about\\ui\\About.tsx"
+			filename: "C:\\Users\\User\\Desktop\\projects\\production-project\\src\\pages\\about\\ui\\About.tsx",
 		},
 		{
 			code: "import Test from \"@/features/test\"",
-			filename: "C:\\Users\\User\\Desktop\\projects\\production-project\\src\\pages\\about\\ui\\About.tsx"
+			filename: "C:\\Users\\User\\Desktop\\projects\\production-project\\src\\pages\\about\\ui\\About.tsx",
+			options: [
+				{
+					alias: "@"
+				}
+			]
 		},
 		{
 			code: "import Test from \"../model/types\"",
@@ -90,7 +47,26 @@ ruleTester.run("check", plugin.rules.check, {
 					message: "This import should be relative"
 				}
 			]
-		}
+		},
+		{
+			code: "import Test from \"@/features/test/model/types\"",
+			filename: "C:\\Users\\User\\Desktop\\projects\\production-project\\src\\features\\test\\ui",
+			errors: [
+				{
+					message: "This import should be relative"
+				}
+			],
+			options: [
+				{
+					alias: "@"
+				}
+			]
+		},
+		// {
+		// 	code: "import Test from \"features/test\"",
+		// 	filename: "C:\\Users\\User\\Desktop\\projects\\production-project\\src\\pages\\about\\ui\\About.tsx",
+		// 	options: ["asdsadsa"]
+		// },
 	]
 });
 
